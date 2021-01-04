9893934950d5923f1b5617bee1e493f5214012d9
OaCantona <duringjulian@gmail.com>
2021-01-04 22:29:58 +0100

    Finalize model and save result as pin

--- R/pre_process_act.R
+++ R/pre_process_act.R
@@ -1,11 +1,10 @@
-pre_process_act <- function(df_act_raw, complete_rate) {
+pre_process_act <- function(df_act_raw) {
   df_act_raw %>%
     select(
       id, distance, moving_time, elapsed_time, total_elevation_gain, type,
       start_date, average_heartrate, elev_high, athlete_id,
-      elev_low, average_temp, max_speed, average_speed, average_cadence
+      elev_low, max_speed, average_speed,
     ) %>%
-    select(where(~ sum(!is.na(.x)) / length(.x) >= complete_rate)) %>%
     mutate(start_date = as_date(start_date)) %>%
     relocate(average_heartrate, id)
 }
--- _drake.R
+++ _drake.R
@@ -8,8 +8,7 @@ bpm_plan <- drake_plan(
   complete_rate = 0.9,
 
   df_act_raw = act_raw(),
-  df_act = pre_process_act(df_act_raw, complete_rate),
-  df_act_run = filter(df_act, type == "Run"),
+  df_act = pre_process_act(df_act_raw),
   df_act_id_rel = distinct(df_act, id, athlete_id, type),
   df_act_meas_nested = act_meas_nested(df_act_id_rel),
   df_act_meas = act_meas(df_act_meas_nested, complete_rate),
@@ -19,25 +18,29 @@ bpm_plan <- drake_plan(
   gg_eda_act_meas_ride = vis_eda_act_meas(df_act_meas, "Ride"),
 
   set.seed(123),
-  init_split = initial_split(df_act_run, strata = average_heartrate),
+  init_split = initial_split(df_act, strata = average_heartrate),
   bpm_train = training(init_split),
   bpm_test = testing(init_split),
   set.seed(234),
   bpm_folds = bootstraps(bpm_train, strata = average_heartrate),
 
-  glm_net_recipe = glm_net_rec(df_act_run),
+  glm_net_recipe = glm_net_rec(df_act),
   glm_net_specification = glm_net_spec(),
   glm_net_workflow = glm_net_wf(glm_net_recipe, glm_net_specification),
   glm_net_tune = glm_net_tune_wf(glm_net_workflow, bpm_folds),
   glm_net_fit = glm_net_last_fit(glm_net_workflow, glm_net_tune, init_split),
   gg_glm_net_fit = vis_glm_net_pred(glm_net_fit),
+  gg_glm_net_vip = vis_vip(
+    glm_net_specification, glm_net_tune, glm_net_recipe, "glmnet", bpm_train),
 
-  rf_recipe = rf_rec(df_act_run),
+  rf_recipe = rf_rec(df_act),
   rf_specification = rf_spec(),
   rf_workflow = rf_wf(rf_recipe, rf_specification),
   rf_tune = rf_tune_wf(rf_workflow, bpm_folds),
   rf_fit = rf_last_fit(rf_workflow, rf_tune, init_split),
   gg_rf_fit = vis_glm_net_pred(rf_fit),
+  gg_rf_vip = vis_vip(
+    rf_specification, rf_tune, rf_recipe, "ranger", bpm_train),
 
   bpm_report = target(
     command = {
