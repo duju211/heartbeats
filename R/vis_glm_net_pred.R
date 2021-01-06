26c3685be3e2e65f971d54c197660725df716a88
OaCantona <duringjulian@gmail.com>
2021-01-06 20:59:43 +0100

    Include type of activity in plot

--- R/vis_glm_net_pred.R
+++ R/vis_glm_net_pred.R
@@ -1,6 +1,8 @@
 vis_glm_net_pred <- function(glm_net_fit) {
   collect_predictions(glm_net_fit) %>%
-    ggplot(aes(x = average_heartrate, y = .pred)) +
+    bind_cols(
+      select(testing(first(pull(glm_net_fit, splits))), -average_heartrate)) %>%
+    ggplot(aes(x = average_heartrate, y = .pred, color = type)) +
     geom_point() +
     geom_abline(linetype = 2) +
     coord_fixed()
